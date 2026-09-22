4f0a9639512d8921a112c1fc89dfd0185c4a869f
Paulo Khayat <paulokhayat@gmail.com>
2026-09-21 23:23:59 -0400

    moving point

--- src/shaders/background-frag.glsl
+++ src/shaders/background-frag.glsl
@@ -1,9 +1,14 @@
 out vec4 out_Col;
-
+in vec2 fs_Pos;
 uniform mat4 u_ViewProj;
 
 void main()
 {
-
-    out_Col = vec4(vec3(0.1) + dot((u_ViewProj*(vec4(0.,0.,1.,0.))).xyz, vec3(0.,0.,1.)), 1.);
+    vec2 uv = 0.7 * fs_Pos;
+    vec3 dir =normalize( (u_ViewProj*vec4(0., sin(u_Time) ,cos(u_Time),0.)).xyz);
+    vec3 frwd = normalize(vec3(uv, sqrt(max(0., 1.0 - dot(uv, uv)))));
+    // vec3 frwd = normalize(vec3(uv, 1.0 - (max(abs(uv.x), abs(uv.y)))));
+    float amount = step(0.995,dot(dir, frwd));
+    float drop = step( dot(uv, uv), 1.);
+    out_Col = vec4(vec3(0.1 + drop * amount) , 1.);
 }
--- src/shaders/background-vert.glsl
+++ src/shaders/background-vert.glsl
@@ -2,10 +2,10 @@
 //149
 
 in vec4 vs_Pos;             // The array of vertex positions passed to the shader
-
+out vec2 fs_Pos;
 void main()
 {
-
+    fs_Pos = vs_Pos.xy;
     vec4 modelposition = vs_Pos;   // Temporarily store the transformed vertex positions for use below
 
     gl_Position = modelposition;// gl_Position is a built-in variable of OpenGL which is
