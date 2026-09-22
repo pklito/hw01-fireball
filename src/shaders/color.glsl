935b43cc0e8d2a63d14b47afe47b2a5e8c19fd77
Paulo Khayat <paulokhayat@gmail.com>
2026-09-21 22:09:48 -0400

    finish kelvin heat

--- src/shaders/color.glsl
+++ src/shaders/color.glsl
@@ -4,18 +4,37 @@ float remap(float x, float in1, float in2, float out1, float out2) {
     return mix(out1, out2, t);
 }
 float clamped_remap(float x, float in1, float in2, float out1, float out2){
-    return clamp(remap(x, in1, in2, out1, out2), out1, out2);
+    return clamp(remap(x, in1, in2, out1, out2), min(out1, out2), max(out2, out1));
 }
 vec3 kelvinToColor(float k){
-    //1k - 40k
+    // valid range: 1000K - 40000K
     k /= 100.;
-    float r = mix(255., 329.698727446 * pow(k - 60., -0.1332047592) , step(66., k));
-    float g = mix(99.4708025*log(k) - 161.119581, 288.1221695283 * pow(k-60., -0.0755148492) , step(66., k));
-    float b = 138.517731221 * (log(k-10.) - 305.0447927307);
-    b = mix(0., mix(b, 255., step(66., k)), step(19., k));
 
-    vec3 color = vec3(r,g,b)/255.;
-    color = clamp(color,vec3(0.), vec3(1.));
+    float r = mix(
+        255.,
+        329.698727446 * pow(max(k - 60., 0.0001), -0.1332047592),
+        step(66., k)
+    );
+
+    float g = mix(
+        99.4708025861 * log(max(k, 0.0001)) - 161.1195681661,
+        288.1221695283 * pow(max(k - 60., 0.0001), -0.0755148492),
+        step(66., k)
+    );
+
+    float b = 138.5177312231 * log(max(k - 10., 0.0001)) - 305.0447927307;
+    b = mix(0., mix(b, 255., step(66., k)), step(15., k));
+
+    float factor = 255. - (k-15.)*(k-15.);
+    factor /= 255.;
+    vec3 color = vec3(r, g, b) / 255.;
+    
+    if(k <= 0.){
+        color *= (20. + (10./(k*k +1.))) / 255.;
+    }else if (k <= 15.){
+        color *= factor;
+    }
+    color = clamp(color, vec3(0.), vec3(1.));
 
     return color;
-}    
+}
--- src/shaders/lambert-frag.glsl
+++ src/shaders/lambert-frag.glsl
@@ -16,7 +16,7 @@ out vec4 out_Col; // This is the final output color that you will see on your
 void main()
 {
         float noise = 0.95;
-        vec3 heat = kelvinToColor(clamped_remap(fs_Pos.z, -1.5, 1., 1000., 40000.));
+        vec3 heat = kelvinToColor((remap(fs_Pos.z, -1., 1.5, 5000., -1000.)));
         vec4 diffuseColor = vec4(noise * heat, u_Color.a);
 
         // Calculate the diffuse term for Lambert shading
